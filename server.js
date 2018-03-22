bcd1b9142958ff4738f9dd0ad04950a25627df1f
ssheaks <sarah.sheaks@gmail.com>
2018-03-22 09:02:40 -0700

    update db/seed and create seed-database.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-const { PORT, MONGODB_URI } = require('./config');
+const { MONGODB_URI } = require('./config');
+
+const { PORT } = require('./config');
 
 const notesRouter = require('./routes/notes');
 
