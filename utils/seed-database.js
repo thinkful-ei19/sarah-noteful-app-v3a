620e16405a5dac860d620a2c90d4c7c20748d1e9
ssheaks <sarah.sheaks@gmail.com>
2018-03-23 07:54:13 -0700

    update seed-database.js with tags

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 const Folder = require('../models/folder');
+const Tag = require('../models/tag');
 
 const seedNotes = require('../db/seed/notes');
 const seedFolders = require('../db/seed/folders');
+const seedTags = require('../db/seed/tags');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
@@ -15,7 +17,10 @@ mongoose.connect(MONGODB_URI)
     return Promise.all([
       Note.insertMany(seedNotes),
       Folder.insertMany(seedFolders),
+      Tag.insertMany(seedTags),
+      Note.createIndexes(),
       Folder.createIndexes(),
+      Tag.createIndexes()
     ]);
   })
   .then(() => mongoose.disconnect())
@@ -23,5 +28,21 @@ mongoose.connect(MONGODB_URI)
     console.error(`ERROR: ${err.message}`);
     console.error(err);
   });
+
+
+// mongoose.connect(MONGODB_URI)
+//   .then(() => mongoose.connection.db.dropDatabase())
+//   .then(() => {
+//     return Promise.all([
+//       Note.insertMany(seedNotes),
+//       Folder.insertMany(seedFolders),
+//       Folder.createIndexes(),
+//     ]);
+//   })
+//   .then(() => mongoose.disconnect())
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
 
   
