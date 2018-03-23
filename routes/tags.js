e5ae930c82a9d58254d48354a32a53855e2bc679
ssheaks <sarah.sheaks@gmail.com>
2018-03-23 08:44:19 -0700

    update server.js

--- routes/tags.js
+++ routes/tags.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+// const Folder = require('../models/folder');
+// const Note = require('../models/note');
+const Tag = require('../models/tag');
+
+
+
+
+module.exports = router:
+
